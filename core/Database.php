c2cca43b7c9952c9ec5b633e77bac8d5d0dcb707
shahd <mazouzchahd12@gmail.com>
2025-12-30 17:11:15 +0100

    fix: fix miss spelling db to dbname

--- core/Database.php
+++ core/Database.php
@@ -4,18 +4,21 @@ class Database {
     private static ?pdo $connexion = null;
 
     public static function getconnection(): PDO {
-        if (self::$connexion === null) {
+       if (self::$connexion === null) {
             $config = require __DIR__ . '/../config/database.php';
             try {
-                self::$connexion = new PDO ("mysql : host {$config['host']}; db = {$config['db']}; charset = utf8", $config ['user'], $config ['password']);
+                self::$connexion = new PDO ("mysql:host={$config['host']};port={$config['port']};dbname={$config['db']};charset=utf8", $config ['user'], $config ['password']);
                 self::$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
             catch (PDOException $e) {
                 die("database connection failed try again!". $e->getMessage());
             }
-        }
+       }
         return self::$connexion;
     }
 }
+
+
+
 ?>
  
