d98974568846c7c63a1ca3ab6f7805fb9e4f0efe
shahd <mazouzchahd12@gmail.com>
2025-12-30 10:38:25 +0100

    chore: create Database class and db connexion

---
+++ core/Database.php
@@ -0,0 +1,20 @@
+<?php
+
+class Database {
+    private static ?pdo $connexion = null;
+
+    public static function getconnection(): PDO {
+        if (self::$connexion === null) {
+            $config = require __DIR__ . '/../config/database.php';
+            try {
+                self::$connexion = new PDO ("mysql = host {$config['host']}; db = {$config['db']}; charset = utf8", $config ['user'], $config ['password']);
+                self::$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch (PDOException $e) {
+                die("database connection failed try again!". $e->getMessage());
+            }
+        }
+        return self::$connexion;
+    }
+}
+?>
