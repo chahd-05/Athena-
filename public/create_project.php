ec26138e9a9c194b62c39ea9d4df7b3596dfbe09
shahd <mazouzchahd12@gmail.com>
2025-12-31 16:11:55 +0100

    feat: create projects

---
+++ public/create_project.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require_once __DIR__ . '/project.php';
+
+if(!isset($_SESSION['user_id'])){
+    header('location: login.php');
+    exit();
+}
+
+$message = '';
+
+if($_SERVER['REQUEST_METHOD'] = $_POST){
+    $name = $_POST['project_name'];
+    $desc = $_POST['description'];
+    $project = new Project();
+    $result =$project->create($name, $desc, $_SESSION['user_id']);
+
+    if($result){
+        $message = "Project created successfully";
+    }
+    else{
+        $message = "A problem occurred in the project's creation";
+    }
+}
+?>
+
+<h2>Create a new project</h2>
+<form method="post">
+    <input type="text" name="project_name" placeholder="project name" required>
+    <textarea name="description" placeholder="description"></textarea>
+    <button type="submit">Create</button>
+</form>
+<p><? $messageK?></p>
+<a href="Dashboard.php">Return to Control Panel</a>
